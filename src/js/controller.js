3110d5cd6e6c30f94e738f36d605f3617dd53ede
David Potapkin <davidosimo1@gmail.com>
2019-02-24 23:24:26 +0200

    improve/controller: add vars 1) this.currency; 2) this.tradeValue = null; 3) this.receiveValue = null; 4)     this.currencyGiveName = USD; 5) this.currencyReceiveName = RUR;

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,13 @@
 /* global app, curencyAPI, angular, workWithCurrency */
 (function() {
   app.controller('currencyController', ['currencyAPI', 'workWithCurrency', function(currencyAPI, workWithCurrency) {
+    this.currency = currencyAPI.getResponse();
+    this.nameOfCurrency = [];
 
+    this.tradeValue = null;
+    this.receiveValue = null;
+
+    this.currencyGiveName = 'USD';
+    this.currencyReceiveName = 'RUR';
   }]);
 }());
