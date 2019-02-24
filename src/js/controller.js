0f89450b0f7dcd0d98d25dc1bb0554b84594a714
David Potapkin <davidosimo1@gmail.com>
2019-02-24 23:25:40 +0200

    improve/controller: add method "convertMoney"

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,5 +9,23 @@
 
     this.currencyGiveName = 'USD';
     this.currencyReceiveName = 'RUR';
+
+    this.convertMoney = () => {
+      let result = 0;
+
+      this.currency.forEach(({ ccy: currencyName, buy }) => {
+        if (this.currencyGiveName === 'BTC') {
+          result = workWithCurrency.convertFromBTCtoUAH(this.tradeValue, buy, this.currency[0].buy);
+        } else if (currencyName === this.currencyGiveName) {
+          result = workWithCurrency.convertToUAH(this.tradeValue, buy);
+        }
+      });
+
+      this.currency.forEach(({ ccy: currencyName, sale }) => {
+        if (currencyName === this.currencyReceiveName) {
+          this.receiveValue = workWithCurrency.convertFromUAH(result, sale);
+        }
+      });
+    };
   }]);
 }());
