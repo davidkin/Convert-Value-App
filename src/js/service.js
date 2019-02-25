8329b42cea5b2612084d0553c213b1e558219595
David <davidfloyd322@gmail.com>
2019-02-25 16:31:02 +0200

    fix/service: rename services

--- src/js/service.js
+++ src/js/service.js
@@ -1,7 +1,7 @@
 /* global app, angular */
 
 (function() {
-  app.service('currencyAPI', ['$http', function($http) {
+  app.service('currencyAPIService', ['$http', function($http) {
     this.data = [];
     this.getResponse = () => {
       $http({
@@ -13,7 +13,7 @@
     };
   }]);
 
-  app.service('workWithCurrency', [function() {
+  app.service('workWithCurrencyService', [function() {
     this.convertToUAH = (currentlyVal, buyVal) => Number((currentlyVal * buyVal).toFixed(2));
     this.convertFromUAH = (sumUAH, buyVal) => Number((sumUAH / buyVal).toFixed(2));
     this.convertFromBTCtoUAH = (sumUAH, buyVal, valueUAH) => Number((sumUAH * buyVal * valueUAH).toFixed(2));
