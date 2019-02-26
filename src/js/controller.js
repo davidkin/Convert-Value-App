f857d76f04952517812a5d0522eb14c3bd045219
David <davidfloyd322@gmail.com>
2019-02-26 13:19:28 +0200

    improve/contrller: add watchers

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,43 +1,57 @@
-/* global app, currencyAPIService, angular, workWithCurrencyServiceService */
+/* global app, angular, workWithCurrencyServiceService, constants */
 (function() {
   app.controller('currencyController',
-    ['currencyAPIService', 'workWithCurrencyService', function(currencyAPIService, workWithCurrencyService) {
-      this.currency = currencyAPIService.getResponse();
-      this.nameOfCurrency = [];
+    ['$scope', 'workWithCurrencyService', 'constants',
+      function($scope, workWithCurrencyService, constants) {
+        this.currency = workWithCurrencyService.getResponse();
+        this.nameOfCurrency = [];
 
-      this.tradeValue = null;
-      this.receiveValue = null;
+        this.tradeValue = null;
+        this.receiveValue = null;
+        this.constantsFee = constants.fee;
+        [this.comission] = constants.fee;
 
-      this.currencyGiveName = 'USD';
-      this.currencyReceiveName = 'RUR';
+        this.currencyGiveName = 'USD';
+        this.currencyReceiveName = 'RUR';
 
-      this.convertMoney = () => {
-        let result = 0;
+        this.convertMoney = () => {
+          let result = 0;
 
-        this.currency.forEach(({ ccy: currencyName, buy }) => {
-          if (this.currencyGiveName === 'BTC') {
-            result = workWithCurrencyService.convertFromBTCtoUAH(this.tradeValue, buy, this.currency[0].buy);
-          } else if (currencyName === this.currencyGiveName) {
-            result = workWithCurrencyService.convertToUAH(this.tradeValue, buy);
+          this.currency.forEach(({ ccy: currencyName, buy }) => {
+            if (this.currencyGiveName === 'BTC') {
+              result = workWithCurrencyService.convertFromBTCtoUAH(this.tradeValue, buy, this.currency[0].buy);
+            } else if (currencyName === this.currencyGiveName) {
+              result = workWithCurrencyService.convertToUAH(this.tradeValue, buy);
+            }
+          });
+
+          this.currency.forEach(({ ccy: currencyName, sale }) => {
+            if (currencyName === this.currencyReceiveName) {
+              this.receiveValue = workWithCurrencyService.convertFromUAH(result, sale);
+            }
+          });
+        };
+
+        $scope.$watchGroup(
+          ['cc.currencyReceiveName', 'cc.currencyReceiveName', 'cc.tradeValue'],
+          () => {
+            this.convertMoney();
           }
+        );
+
+        $scope.$watch('cc.comission', () => {
+          this.withCommissions();
         });
 
-        this.currency.forEach(({ ccy: currencyName, sale }) => {
-          if (currencyName === this.currencyReceiveName) {
-            this.receiveValue = workWithCurrencyService.convertFromUAH(result, sale);
-          }
-        });
-      };
+        this.swapCurrency = () => {
+          [this.currencyGiveName, this.currencyReceiveName] = [this.currencyReceiveName, this.currencyGiveName];
+          [this.tradeValue, this.receiveValue] = [this.receiveValue, this.tradeValue];
+        };
 
-      this.swapCurrency = () => {
-        [this.currencyGiveName, this.currencyReceiveName] = [this.currencyReceiveName, this.currencyGiveName];
-        [this.tradeValue, this.receiveValue] = [this.receiveValue, this.tradeValue];
-      };
-
-      this.withCommissions = $event => {
-        this.convertMoney();
-        const comission = workWithCurrencyService.convertWithFee(this.receiveValue, $event.target.value);
-        this.receiveValue = Number((this.receiveValue - comission).toFixed(2));
-      };
-    }]);
+        this.withCommissions = () => {
+          this.convertMoney();
+          const resWithFee = workWithCurrencyService.convertWithFee(this.receiveValue, this.comission);
+          this.receiveValue = Number((this.receiveValue - resWithFee).toFixed(2));
+        };
+      }]);
 }());
