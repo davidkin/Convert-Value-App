5646d03a196a0a9b72cf825a4b19e449c214e60f
David <davidfloyd322@gmail.com>
2019-02-28 13:40:25 +0200

    improve/app: add one more state

--- src/js/app.js
+++ src/js/app.js
@@ -8,15 +8,22 @@
     function(workWithCurrencyServiceProvider, $stateProvider) {
       workWithCurrencyServiceProvider.congigurateAPI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
 
-      $stateProvider.state({
-        name: 'converter',
-        url: '/converter',
-        templateUrl: 'curency-converter.html'
-      }).state({
-        name: 'home',
-        url: '/home',
-        template: '<h2 class="h2">Home Page</h2>'
-      });
+      $stateProvider
+        .state({
+          name: 'converter',
+          url: '/converter',
+          templateUrl: 'curency-converter.html'
+        })
+        .state({
+          name: 'home',
+          url: '/home',
+          template: '<h2 class="h2">Home Page</h2>'
+        })
+        .state({
+          name: 'app',
+          url: '',
+          template: '<h2 class="h2">Welcome to my App</h2>'
+        });
     }]);
 
   app.run(function($rootScope) {
